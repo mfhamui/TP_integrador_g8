fc75884d73e497492baaf834349030d344270f46
felih <hamuim@udesa.edu.ar>
2024-11-18 13:05:52 -0300

    algunos cambios

--- js/register.js
+++ js/register.js
@@ -3,15 +3,16 @@ let contraseña=document.querySelector(`.contra`);
 let form = document.querySelector('.bro');
 
 
-formorm.addEventListener ("submit", function(event){
+form.addEventListener ("submit", function(event){
     event.preventDefault (); 
     if (email.value == ""){
         prompt("Por favor complete el campo");
+        return;
         
     }
     if (contraseña.value == "") {
         prompt (" Por favor complete el campo");
-        
+        return;
     }
     else{ this.submit()}
 
