d4473646bcafdfc0fb966543235bdaca55cae0ea
felih <hamuim@udesa.edu.ar>
2024-11-18 18:12:43 -0300

    hice el js e index de categories

---
+++ js/categories.js
@@ -0,0 +1,31 @@
+let qs= location.search;
+let qsObj = new URLSearchParams(qs);
+let info = qsObj.get(`q`);
+const URL = `https://dummyjson.com/recipes/tags`
+
+let cat= document.querySelector(".categories"); 
+
+
+fetch (URL)
+.then(function (response) {
+    return response.json();
+})
+
+.then(function (data) {
+    console.log(data); 
+    let categoria= "";
+     
+
+    for (let i = 0; i < data.length; i++) {
+        categoria += 
+            `<article>
+                <a href="./category.html?id=${data[i]}"> ${data[i]} </a> 
+            </article>`
+    }; 
+    cat.innerHTML = categoria;
+   
+})
+.catch(function (error) {
+    return console.log(error);
+    
+});
--- js/register.js
+++ js/register.js
@@ -4,7 +4,8 @@ let form = document.querySelector('.bro');
 
 /*esto esta mal el prompt pide qeu llenes y la consigna no pide eso, esto dice la consigna: 
 Validar el campo email. Si está vacío mostrar al usuario el texto en pantalla (no con alert) 
-"Por favor complete el campo" y el formulario no se enviará.
+"Por favor complete el campo" y el formulario no se enviará.*/ 
+
 
 form.addEventListener ("submit", function(event){
     event.preventDefault (); 
@@ -20,4 +21,3 @@ form.addEventListener ("submit", function(event){
     else{ this.submit()}
 
 });
-*/ 
