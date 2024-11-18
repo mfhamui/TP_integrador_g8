e3afab2462c823bd0dacbb327f87a70a0a4fa09b
felih <hamuim@udesa.edu.ar>
2024-11-18 13:13:03 -0300

    marque un error

--- js/register.js
+++ js/register.js
@@ -2,6 +2,9 @@ let email = document.querySelector(`.email`);
 let contraseña=document.querySelector(`.contra`);
 let form = document.querySelector('.bro');
 
+/*esto esta mal el prompt pide qeu llenes y la consigna no pide eso, esto dice la consigna: 
+Validar el campo email. Si está vacío mostrar al usuario el texto en pantalla (no con alert) 
+"Por favor complete el campo" y el formulario no se enviará.
 
 form.addEventListener ("submit", function(event){
     event.preventDefault (); 
@@ -17,3 +20,4 @@ form.addEventListener ("submit", function(event){
     else{ this.submit()}
 
 });
+*/ 
