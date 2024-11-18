84c7cbeeb806702f00006d771896b2b55f103250
felih <hamuim@udesa.edu.ar>
2024-11-18 12:58:04 -0300

    cambie el register js que tambien tenia errores

--- js/register.js
+++ js/register.js
@@ -1,16 +1,18 @@
-let email = document.querySelector(`input[name="email]`);
-let contraseña=document.querySelector(`input [name="contra"]`);
-let form = document.querySelector('form');
+let email = document.querySelector(`.email`);
+let contraseña=document.querySelector(`.contra`);
+let form = document.querySelector('.bro');
 
 
-Form.addEventListener ("submit", function(event){
+formorm.addEventListener ("submit", function(event){
+    event.preventDefault (); 
     if (email.value == ""){
         prompt("Por favor complete el campo");
-        event.preventDefault (); // esto es para que no se mande el form si esta vacio //
+        
     }
     if (contraseña.value == "") {
         prompt (" Por favor complete el camp");
-        event.preventDefault();
+        
     }
+    else{ this.submit()}
 
 });
