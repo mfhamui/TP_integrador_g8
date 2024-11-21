96773de8120db4b454edde49a50ea3f3b9e97060
morenadelmastro <mdelmastrocoll@udesa.edu.ar>
2024-11-21 18:03:55 -0300

    java del registro intento

--- js/register.js
+++ js/register.js
@@ -1,22 +1,24 @@
 const form = document.querySelector('.bro');
 const emailTexto= document.querySelector('.email');
 const contraTexto = document.querySelector('.contra');
-
+let mensajeError= document.getElementById("mensaje-error");
 
 form.addEventListener ('submit', function(event){
     event.preventDefault();
     if (email.value ==" "){
-        confirm("Por favor complete el campo")
-        console.log("email vacio");
-        return;
+        mensajeError.textContent= "Por favor complete el campo email";    
+    }else{
+        mensajeError.textContent=" ";
+    this.submit();
     }
-    if (contraseña.value.trim() === " "){
-        confirm ("Por favor complete el campo")
-        console.log("contrasena vacio");
-        return;
+    
+    if(contraseña.value ==" "){
+        mensajeError.textContent ="Por favor complete el campo email"; 
+    }else{
+        mensajeError.textContent=" ";
+    this.submit();
     }
-    console.log("estan vacios los campos,completelos ")
-    this.submit ();
+   
 });  
 
 
