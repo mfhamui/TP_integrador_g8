ed818178f189378484f402cd35846d683e912623
morenadelmastro <mdelmastrocoll@udesa.edu.ar>
2024-11-23 18:53:18 -0300

    terminado el java de registro

--- js/register.js
+++ js/register.js
@@ -2,17 +2,22 @@ let form = document.querySelector('.bro');
 let emailTexto = document.querySelector('.email');
 let contraTexto = document.querySelector('.contra');
 let mensajeError = document.querySelector("#mensaje-error");
-let checkbox = document.querySelector('#t&c')
+let checkbox = document.querySelector("#check");
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
     if (emailTexto.value == "") {
         mensajeError.textContent = "Por favor complete el campo email";
-    } else if (contra.value == "") {
+        return;
+    } 
+    if (contraTexto.value == "") {
         mensajeError.textContent = "Por favor complete el campo contraseña";
-    } else {
-        mensajeError.textContent = "";
-        this.submit();
+        return;
     }
-    
+    if (checkbox.checked== false){
+         mensajeError.textContent="acepte terminos y condiciones";
+        return;
+    }
+    mensajeError.textContent="";
+    form.submit();
 });
