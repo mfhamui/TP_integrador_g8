9adc63cedc61505dee3be436e22a7e042bfb0ff3
jcosentinoa <cosentinoj@udesa.edu.ar>
2024-11-25 13:25:20 -0300

    arreglo dos

--- js/searchResults.js
+++ js/searchResults.js
@@ -31,32 +31,11 @@ fetch(URL)
         sr.innerHTML = character;
 
     })
-    
-    .catch (function (error) {
-    return console.log(error);
 
-});
+    .catch(function (error) {
+        return console.log(error);
 
-if (info) {
-    fetchRecipes(info);
-}
-
-// Escucha el evento de entrada en el campo de búsqueda
-search.addEventListener('input', function (event) {
-    let valor = search.value; // Obtiene el valor actual del campo de búsqueda
-    if (valor.trim() === "") {
-        sr.innerText = `Error: Por favor ingresa un término de búsqueda.`;
-        return; // Salimos si no hay valor
-    } else {
-        sr.innerText = ""; // Limpia el mensaje de error
-    }
-
-    // Realiza la búsqueda con el nuevo valor
-    fetchRecipes(valor);
-});
-
-
-
+    });
 
 
 
@@ -65,31 +44,16 @@ formbusca.addEventListener('submit', function (event) {
     event.preventDefault();
     const searchTerm = search.value;
 
-<<<<<<< HEAD
-        if (searchTerm === '') {
-            alert(' ERROR Por favor, ingresa un término de búsqueda.');
-            return;
-        }
-
-        if (searchTerm.length < 3) {
-            alert('ERROR, El término de búsqueda debe tener al menos 3 caracteres.');
-            return;
-        }
-
-        else{ this.submit()}
-    });  
-=======
     if (searchTerm === '') {
-        alert('errorMessage', 'Por favor, ingresa un término de búsqueda.');
+        alert(' ERROR Por favor, ingresa un término de búsqueda.');
         return;
     }
 
     if (searchTerm.length < 3) {
-        alert('errorMessage', 'El término de búsqueda debe tener al menos 3 caracteres.');
+        alert('ERROR, El término de búsqueda debe tener al menos 3 caracteres.');
         return;
     }
 
     else { this.submit() }
 });
->>>>>>> ed818178f189378484f402cd35846d683e912623
 
